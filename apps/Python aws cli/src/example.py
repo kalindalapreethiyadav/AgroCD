eec70ae74b68ef26af96bf3e1ead23754d20d3a9
Preethi Kalindala <kpreethiyadav579@gmail.com>
2023-02-15 21:28:57 +0530

    update adding

--- apps/Python aws cli/src/example.py
+++ apps/Python aws cli/src/example.py
@@ -1,10 +1,14 @@
-#!/bin/bash
 import boto3
 
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('my-bucket')
-for obj in bucket.objects.all():
-    print(obj.key)
+def s3_client():
+    s3 = boto3.client('s3')
+    """ :type : pyboto3.s3 """
+    return s3
+
+def s3_resource():
+    s3 = boto3.resource('s3')
+    return s3
+
 
 def create_bucket(bucket_name):
     return s3_client().create_bucket(
@@ -13,3 +17,4 @@ def create_bucket(bucket_name):
             'LocationConstraint': 'eu-central-1'
         }
     )
+
