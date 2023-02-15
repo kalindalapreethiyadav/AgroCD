8f0d0be1f565b83fb41f2e55133fa003fb6110ab
Preethi Kalindala <kpreethiyadav579@gmail.com>
2023-02-15 23:20:46 +0530

    update adding

--- apps/Python aws cli/src/download_file.py
+++ apps/Python aws cli/src/download_file.py
@@ -2,16 +2,18 @@ import boto3
 import os
 
 BUCKET_NAME = 'mybucketpreek'
+Object_File_Name = 'sub_app/helm-charts-main.gz.zip'
+Local_file_Name = 'download.zip'
 
 def s3_client():
     s3 = boto3.client('s3')
     """ :type : pyboto3.s3 """
     return s3
 
-def downlaod_small_file():
+def downlaod_file():
     #file_path = os.path.dirname(__file__) + 'sub_app/helm-charts-main.gz.zip'
-    return s3_client().download_file(BUCKET_NAME, 'sub_app/helm-charts-main.gz.zip')
+    return s3_client().download_file(BUCKET_NAME, Object_File_Name, Local_file_Name)
 
 #main function start
 if __name__ == '__main__':
-    print(downlaod_small_file())
+    print(downlaod_file())
